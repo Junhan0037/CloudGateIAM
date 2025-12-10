b1ea1e9ecb8404676967817de3d1f0037ae4ac87
Junhan0037 <wnsgks0037@naver.com>
2025-12-10 13:09:34 +0900

    Initial commit

---
+++ src/main/kotlin/com/cloudgateiam/CloudGateIamApplication.kt
@@ -0,0 +1,11 @@
+package com.cloudgateiam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CloudGateIamApplication
+
+fun main(args: Array<String>) {
+    runApplication<CloudGateIamApplication>(*args)
+}
---
+++ src/test/kotlin/com/cloudgateiam/CloudGateIamApplicationTests.kt
@@ -0,0 +1,7 @@
+package com.cloudgateiam
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CloudGateIamApplicationTests {
+}
